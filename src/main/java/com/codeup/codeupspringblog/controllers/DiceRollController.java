2b025bd408a16461f48e310c4a5f8885347c2f7d
emata10 <130502916+emata10@users.noreply.github.com>
2023-08-07 11:30:41 -0600

    DiceRollController,roll-dice.html, roll-result.html

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/DiceRollController.java
@@ -0,0 +1,40 @@
+package com.codeup.codeupspringblog.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Random;
+
+@Controller
+@RequestMapping
+public class DiceRollController {
+
+    @GetMapping("/roll-dice")
+    public String showRollDicePage() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDice(@PathVariable int guess, Model model) {
+        int diceRoll = roll();
+        model.addAttribute("diceRoll", diceRoll);
+        model.addAttribute("userGuess", guess);
+        model.addAttribute("message", getMessage(guess, diceRoll));
+        return "roll-result";
+    }
+
+    private int roll() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+
+    private String getMessage(int guess, int diceRoll) {
+        if (guess == diceRoll) {
+            return "Congratulations! You guessed correctly!";
+        } else {
+            return "Oops! Your guess was incorrect.";
+        }
+    }
+}
