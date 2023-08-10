05d6bea3c28e2bed8179752a00cf9df26741f438
emata10 <130502916+emata10@users.noreply.github.com>
2023-08-10 10:37:48 -0600

    added findPostById to the PostRepository

--- src/main/java/com/codeup/codeupspringblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/codeupspringblog/repositories/PostRepository.java
@@ -4,17 +4,6 @@ import com.codeup.codeupspringblog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    class PostController {
-        private final PostRepository adDao;
+    Post findPostById(long id );
+ }
 
-        public PostController(PostRepository adDao) {
-            this.adDao = adDao;
-        }
-
-        public PostRepository getAdDao() {
-            return adDao;
-        }
-    }
-
-
-}
--- src/main/java/com/codeup/codeupspringblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/codeupspringblog/repositories/UserRepository.java
@@ -3,6 +3,5 @@ package com.codeup.codeupspringblog.repositories;
 import com.codeup.codeupspringblog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserById(long id);
-    User findUserByUsername(String username);
+
 }
