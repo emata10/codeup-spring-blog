274e76408880bffe84877b5c9a38042c41041543
emata10 <130502916+emata10@users.noreply.github.com>
2023-08-11 15:23:17 -0600

    AdUserController

--- src/main/java/com/codeup/codeupspringblog/controllers/AdUserController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/AdUserController.java
@@ -1,8 +1,7 @@
 package com.codeup.codeupspringblog.controllers;
 
 import com.codeup.codeupspringblog.model.AdUser;
-import com.codeup.codeupspringblog.model.User;
-import com.codeup.codeupspringblog.repositories.UserRepository;
+import com.codeup.codeupspringblog.repositories.AdUserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -14,22 +13,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 @AllArgsConstructor
 @Controller
 public class AdUserController {
-    private UserRepository userDao;
+    private AdUserRepository userDao;
     private PasswordEncoder passwordEncoder;
-    private User adUser;
-
 
     @GetMapping("/sign-up")
-    public String showSignupForm(Model model){
+    public String showSignupForm(Model model) {
         model.addAttribute("user", new AdUser());
         return "users/sign-up";
     }
 
     @PostMapping("/sign-up")
-    public String saveUser(@ModelAttribute AdUser user){
+    public String saveUser(@ModelAttribute AdUser user) {
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
-        userDao.save(adUser);
+        userDao.save(user);
         return "redirect:/login";
     }
 }
